d24598a8ca0ced887c0aef6ec17b073cc7e4be21
BleuSky03 <taksin@photomann.com>
2023-04-01 16:58:03 -0400

    Spring implementation

--- Assets/Scripts/Shootables/MemoryFragmentShootable.cs
+++ Assets/Scripts/Shootables/MemoryFragmentShootable.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class MemoryFragmentShootable : MonoBehaviour, IShootable
 {
     [SerializeField] private int amountOfHitsNeeded;
+    [Range(0, 100)]
+    [SerializeField] private int hitSizeIncrease;
+    private float sizeScale;
+    private SpringValue spring;
+
+    [Header("Spring Settings")]
+    [SerializeField] private float stiffness;
+    [SerializeField] private float damping;
+    
+
+    private void Start()
+    {
+        spring = new SpringValue(this.transform.localScale.x, stiffness, damping);
+        sizeScale = (hitSizeIncrease / 100) * transform.localScale.x;
+    }
+    private void Update()
+    {
+        spring.Update(Time.deltaTime);
+        float temp = Mathf.Abs(spring.value);
+        transform.localScale = new Vector3(temp, temp, temp);
+    }
+
     public void Hit()
     {
         amountOfHitsNeeded -= 1;
-        transform.localScale *= 1.1f;
+        spring.targetValue += sizeScale;
+        spring.Nudge(1);
         if (amountOfHitsNeeded <= 0)
         {
-            Destroy(gameObject);
+            spring.targetValue = 0;
+            Destroy(gameObject, 2);
             //enter behavior to change level
         }
     }
