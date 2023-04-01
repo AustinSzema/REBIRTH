93640ca4911f13a8c09a171744e06131b8c28810
BleuSky03 <taksin@photomann.com>
2023-03-31 23:01:04 -0400

    Update MemoryFragmentShootable.cs

--- Assets/Scripts/Shootables/MemoryFragmentShootable.cs
+++ Assets/Scripts/Shootables/MemoryFragmentShootable.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class MemoryFragmentShootable : MonoBehaviour, IShootable
 {
+    [SerializeField] private int amountOfHitsNeeded;
     public void Hit()
     {
-        throw new System.NotImplementedException();
+        amountOfHitsNeeded -= 1;
+        if (amountOfHitsNeeded <= 0)
+        {
+            Destroy(this);
+            //enter behavior to change level
+        }
     }
 
     // Start is called before the first frame update
