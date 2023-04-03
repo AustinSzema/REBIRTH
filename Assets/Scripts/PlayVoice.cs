f1f8d86851a050a18d6633824c2b43ff10b05ed4
Bosspug15 <eugeniojlopezt@gmail.com>
2023-04-02 20:08:05 -0400

    some va

--- Assets/Scripts/PlayVoice.cs
+++ Assets/Scripts/PlayVoice.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class PlayVoice : MonoBehaviour
 {
+    [SerializeField] private Voice voiceSO;
+    [SerializeField] private UnityEvent voice_Transition;
+    [SerializeField] private float voiceDelay = 6f;
+    [SerializeField] private bool isVoiceTransitionScene;
+    int voiceIndex = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioSource.PlayClipAtPoint(voiceSO.VoiceActing[voiceIndex], transform.position);
+        InvokeRepeating("nextAudioClip", 7.0f, voiceDelay);
     }
 
-    // Update is called once per frame
-    void Update()
+    void nextAudioClip()
     {
-        
+        if (voiceIndex <= voiceSO.VoiceActing.Length - 1)
+        {
+            voiceIndex++;
+            AudioSource.PlayClipAtPoint(voiceSO.VoiceActing[voiceIndex], transform.position);
+           
+        }
     }
+
 }
